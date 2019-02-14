b2428f9adf2f6c1c3de9bad37803c3b9e05d0384
keralex <kerlis.aguado@correo.unimet.edu.ve>
2019-02-14 10:13:07 -0400

    prueba hilo cocineros

--- src/operativos1/Cocina.java
+++ src/operativos1/Cocina.java
@@ -11,20 +11,28 @@ import java.io.IOException;
  *
  * @author kagua
  */
-public class Cocina {
+public class Cocina  {
 //Atributos
     
-    CocineroEntrada[] cocinerosEntrada;
-    CocineroFuerte[] cocinerosFuerte;
-    CocineroPostre[] cocinerosPostre;
+    Cocinero[] cocinerosEntrada;
+    Cocinero[] cocinerosFuerte;
+    Cocinero[] cocinerosPostre;
     Mesonero[] mesoneros;
     JefeMesonero jefeMesonero;
     Gerente gerente;
     int cantCocinerosEntrada;
     int cantCocinerosPostre;
     int cantCocinerosFuerte;
+    int cantMaxCocinerosEntrada;
+    int cantMaxCocinerosPostre;
+    int cantMaxCocinerosFuerte;
     int cantMesoneros;
-    int horasTrabajo;
+    float horaMesonero;
+    float horaCocinerosEntrada;
+    float horaCocinerosFuerte;
+    float horaCocinerosPostre;
+
+    int horasRestaurant;
     String filename="C:\\Users\\kagua\\Documents\\NetBeansProjects\\Operativos_Proyecto_1\\src\\operativos1\\text.txt";
     LeerArchivos archivos;
  
@@ -35,10 +43,37 @@ public class Cocina {
     
     public Cocina() throws IOException {
         this.archivos = new LeerArchivos(this.filename);
+        this.cantCocinerosEntrada=this.archivos.cant_CocinerosEntradas;
+        this.cantCocinerosFuerte=this.archivos.cant_CocinerosFuerte;
+        this.cantCocinerosPostre=this.archivos.cant_CocinerosPostre;
+        this.cantMaxCocinerosEntrada=this.archivos.cant_max_CocinerosEntradas;
+        this.cantMaxCocinerosFuerte=this.archivos.cant_max_CocinerosFuerte;
+        this.cantMaxCocinerosPostre=this.archivos.cant_max_CocinerosPostre;
+        this.cantMesoneros=this.archivos.cant_mesoneros;
+        this.horaMesonero=this.archivos.cant_Horas_Mesonero;
+        this.horaCocinerosEntrada=this.archivos.cant_Horas_CocineroEntrada;
+        this.horaCocinerosFuerte=this.archivos.cant_Horas_CocineroFuerte;
+        this.horaCocinerosPostre=this.archivos.cant_Horas_CocineroPostre;
+        
+        
+        this.cocinar();
+        
         
     }
     
-    
+    public void cocinar(){
+        this.cocinerosEntrada=new Cocinero[this.cantCocinerosEntrada];
+        //inicializar cocineros 
+        for(int i=0;i<this.cocinerosEntrada.length;i++){
+            this.cocinerosEntrada[i]=new Cocinero(this.horaCocinerosEntrada,20);
+            
+        }
+        Cocinero cocinero=new Cocinero(this.horaCocinerosFuerte,20);
+        cocinero.start();
+        cocinero.run();
+    }
+
+
     
     
 }
--- src/operativos1/Cocinero.java
+++ src/operativos1/Cocinero.java
@@ -5,12 +5,75 @@
  */
 package operativos1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author kagua
  */
-public abstract class Cocinero {
+public class Cocinero extends Thread {
     int capacidadMeson;
     Meson[] mesones;
+    int horaProducion;
+    
+    
+    //constructor
+    public Cocinero(float horas, int meson) {
+               
+                this.capacidadMeson=meson;
+                this.horaProducion=(int)(horas*100);
+                this.mesones=new Meson[this.capacidadMeson];
+
+    }
+    
+    
+    //Funcionews
+    
+    
+    public void cocinar(){
+        System.out.println("cocinero cocina");
+    }
+    
+    public void esperarSegundos(int segundos) throws InterruptedException{
+        try{
+            System.out.println("Cocinando");
+            Thread.sleep(segundos*1000);
+        }catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+    }
+    
+    //Public Void run y star 
+    
+    public void run(){
+        this.cocinar();
+        try {
+            this.esperarSegundos(this.horaProducion);
+           
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Cocinero.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Termino de cocinar");
+    }
+    
+  
+    //Getters y Setters
+    public int getCapacidadMeson() {
+        return capacidadMeson;
+    }
+
+    public void setCapacidadMeson(int capacidadMeson) {
+        this.capacidadMeson = capacidadMeson;
+    }
+
+    public Meson[] getMesones() {
+        return mesones;
+    }
+
+    public void setMesones(Meson[] mesones) {
+        this.mesones = mesones;
+    }
+    
     
 }
